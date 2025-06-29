fbda85de882540fc5c00cec6da2ee75e79b9fb0c
atodev <tom@tbutler.net>
2025-06-29 12:12:31 +1200

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+items = []
+
+class Item(BaseModel):
+    name: str
 
 @app.get("/")
 def root():
     return {"Hello": "World"}   
+
+@app.post("/items/")
+def create_item(item: Item):
+    items.append(item.name)
+    return {"item": item.name}
+@app.get("/items/")
+def read_items():
+    return {"items": items} 
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    if item_id < len(items):
+        return {"item": items[item_id]}
+    return {"error": "Item not found"}, 404     
